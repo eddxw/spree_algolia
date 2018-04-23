28279e7864d68b0aa5ac7b06627a0eae111c805c
eddxw <eduardo.jemb@gmail.com>
2018-04-23 03:35:30 -0700

    new atributtes

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -3,12 +3,18 @@ Spree::Product.class_eval do
 
   algoliasearch index_name: 'athspree' do
     attribute :name, :sku, :ean
-    attribute :foto, :product_url
+    attribute :foto, :product_url, :descripcion, :precio
   end
   def foto
     master.images.first.nil? ? "https://s3.us-east-2.amazonaws.com/athermosillov2/Imagen+no+disponible.jpg": master.images.first.attachment.url(:small)
   end
   def product_url
     Spree::Core::Engine.routes.url_helpers.product_path(self)
+  end
+  def descripcion
+    strip_tags(:description)[0..200]
+  end
+  def precio
+    cvp = master.volume_prices? master.volume_prices.collect{|cvp| cvp.amount}.sort.reverse : [master.price]
   end
 end
