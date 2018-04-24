4a3ec3a12a0c017be5279ab4ceff8efa24eb0d81
eddxw <eduardo.jemb@gmail.com>
2018-04-24 09:05:40 -0700

    new atributtes

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -13,7 +13,7 @@ Spree::Product.class_eval do
     Spree::Core::Engine.routes.url_helpers.product_path(self)
   end
   def descripcion
-    strip_tags(:description)[0..200]
+    strip_tags(description)[0..200]
   end
   def precio
     cvp = master.volume_prices.any? ? master.volume_prices.collect{|cvp| cvp.amount}.sort.reverse : [master.price]
